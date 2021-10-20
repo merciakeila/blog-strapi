ee8e2b25ae9ee6a3cc9071cf388a11fedbe30bb8
merciakeila <merciakeila@outlook.com>
2021-10-20 07:41:24 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a9d244d859a47af0d16df6202fa1d49e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '34461f17-2c24-4ec5-95f6-e8476775b495'
+};
